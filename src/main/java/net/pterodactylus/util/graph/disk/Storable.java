7d2a09a00da87d069af4dd5acea3c70559d91b97
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2011-02-12 16:07:47 +0100

    Add more helper methods to Storable.Utils.

--- src/main/java/net/pterodactylus/util/graph/disk/Storable.java
+++ src/main/java/net/pterodactylus/util/graph/disk/Storable.java
@@ -40,15 +40,39 @@ public interface Storable {
 
 	static class Utils {
 
-		public static void copyLong(long value, byte[] bytes, int position) {
+		public static char getChar(byte[] buffer, int position) {
+			return (char) (buffer[position + 0] | (buffer[position + 1] << 8));
+		}
+
+		public static int getInt(byte[] buffer, int position) {
+			return (buffer[position + 0] & 0xff) | ((buffer[position + 1] << 8) & 0xff00) | ((buffer[position + 2] << 16) & 0xff0000) | ((buffer[position + 3] << 24) & 0xff000000);
+		}
+
+		public static long getLong(byte[] buffer, int position) {
+			return (buffer[position + 0] & 0xff) | ((buffer[position + 1] << 8) & 0xff00) | ((buffer[position + 2] << 16) & 0xff0000) | ((buffer[position + 3] << 24) & 0xff000000) | ((buffer[position + 4] << 32) & 0xff00000000L) | ((buffer[position + 5] << 40) & 0xff0000000000L) | ((buffer[position + 6] << 48) & 0xff000000000000L) | ((buffer[position + 7] << 56) & 0xff00000000000000L);
+		}
+
+		public static void putChar(char value, byte[] buffer, int position) {
+			buffer[position + 0] = (byte) (value & 0xff);
+			buffer[position + 1] = (byte) ((value >>> 8) & 0xff);
+		}
+
+		public static void putInt(int value, byte[] buffer, int position) {
+			buffer[position + 0] = (byte) (value & 0xff);
+			buffer[position + 1] = (byte) ((value >>> 8) & 0xff);
+			buffer[position + 2] = (byte) ((value >>> 16) & 0xff);
+			buffer[position + 3] = (byte) ((value >>> 24) & 0xff);
+		}
+
+		public static void putLong(long value, byte[] bytes, int position) {
 			bytes[position + 0] = (byte) (value & 0xff);
-			bytes[position + 1] = (byte) ((value >> 8) & 0xff);
-			bytes[position + 2] = (byte) ((value >> 16) & 0xff);
-			bytes[position + 3] = (byte) ((value >> 24) & 0xff);
-			bytes[position + 4] = (byte) ((value >> 32) & 0xff);
-			bytes[position + 5] = (byte) ((value >> 40) & 0xff);
-			bytes[position + 6] = (byte) ((value >> 48) & 0xff);
-			bytes[position + 7] = (byte) ((value >> 56) & 0xff);
+			bytes[position + 1] = (byte) ((value >>> 8) & 0xff);
+			bytes[position + 2] = (byte) ((value >>> 16) & 0xff);
+			bytes[position + 3] = (byte) ((value >>> 24) & 0xff);
+			bytes[position + 4] = (byte) ((value >>> 32) & 0xff);
+			bytes[position + 5] = (byte) ((value >>> 40) & 0xff);
+			bytes[position + 6] = (byte) ((value >>> 48) & 0xff);
+			bytes[position + 7] = (byte) ((value >>> 56) & 0xff);
 		}
 
 	}
